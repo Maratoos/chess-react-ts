1885684d7fa5e9812638ec9a910efe34ee9b1aaf
Maratoos <kudushev01@mail.ru>
2022-05-17 18:31:56 +0600

    Now you can move the figures

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { Board } from '../models/Board'
+import { Cell } from '../models/Cell'
+import { Figure } from '../models/figures/Figure'
 import CellComponent from './CellComponent'
 
 interface BoardProps{
@@ -8,12 +10,42 @@ interface BoardProps{
 }
 
 const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  const [selectedCell, setCelectedCell] = useState<Cell | null>(null)
+
+  function click(cell: Cell) {
+    if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+      selectedCell.moveFigure(cell)
+      setCelectedCell(null)
+      updateBoard()
+    } else {
+      setCelectedCell(cell)
+    }
+  }
+
+  useEffect(() => {
+    highLightCells()
+  }, [selectedCell])
+
+  function highLightCells() {
+    board.highLightCells(selectedCell)
+    updateBoard()
+  }
+
+  function updateBoard() {
+    const newBoard = board.getCopyBoard()
+    setBoard(newBoard)
+  }
+
   return (
     <div className='board'>
       {board.cells.map((row, index) => 
       <React.Fragment key={index}>
         {row.map(cell =>
-          <CellComponent key={cell.id} cell={cell} />  
+          <CellComponent
+          key={cell.id} 
+          cell={cell} 
+          click={click} 
+          selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y} />  
         )}
       </React.Fragment>
       )}
